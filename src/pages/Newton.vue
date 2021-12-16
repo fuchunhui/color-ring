5b7ae0dbdba497dc128072c175e35200fb168646
fuchunhui <fuchuhu7@gmail.com>
2021-12-16 12:25:35 +0800

    feat: new page

---
+++ src/pages/Newton.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    newton
+  </div>
+</template>
+
+<style lang="less">
+
+</style>
---
+++ src/pages/Twelve.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    abbb
+  </div>
+</template>
+
+<style lang="less">
+
+</style>
