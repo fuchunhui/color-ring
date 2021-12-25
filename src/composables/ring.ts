e9376f23b1c12dd90dd2c2377098cce00f3cce2c
fuchunhui <fuchuhu7@gmail.com>
2021-12-25 16:51:26 +0800

    wip: ring

--- src/composables/ring.ts
+++ src/composables/ring.ts
@@ -1,12 +1,35 @@
 import Color from '../base/color';
+import {RING_OPTIONS} from '../types';
 
-const ring = (colors: Color[], deep: number, ctx: CanvasRenderingContext2D): void => {
-  const x = 25 + 2 * 50;
-  const y = 25 + 2 * 50;
-  const r = 100;
-  const r2 = 80;
+const WIDTH = 30;
+const RADIUS = 200;
+const AROUND = 10;
 
-  ctx.lineWidth = 20;
+const ring = (colors: Color[], deep: number = 1, ctx: CanvasRenderingContext2D, options: RING_OPTIONS): void => {
+  if (!colors.length || deep < 1) {
+    return;
+  }
+
+  const lineWidth = options?.width ?? WIDTH;
+  console.log('默认的linewidth为：', lineWidth);
+
+  const r = options?.minr ?? RADIUS;
+  console.log('默认的最小半径为为：', r);
+
+  // 计算colors 的个数
+  // 把圆分成多少份，循环绘制每一层
+  // 根据deep的层数，把圆分成多少层，从最内层开始向外颜色加深。
+  // 最外层为alpha 1，加一次，颜色alpha小于10%。
+
+
+  const x = r + deep * lineWidth + AROUND;
+  const y = x;
+
+  ctx.lineWidth = lineWidth;
+
+  // 循环画圆。
+
+
 
   ctx.beginPath();
   ctx.strokeStyle = 'red';
---
+++ src/types/ring.ts
@@ -0,0 +1,5 @@
+export interface RING_OPTIONS {
+  minr?: number;
+  width?: number;
+  alphaStep?: number;
+}
