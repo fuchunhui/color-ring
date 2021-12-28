caa073ddbf2ea321ac94684ddac1441a54c44eb3
fuchunhui <fuchuhu7@gmail.com>
2021-12-28 21:42:28 +0800

    wip: hexagon

--- src/composables/palette.ts
+++ src/composables/palette.ts
@@ -1,30 +1,14 @@
 import Color from '../base/color';
 import {make, mix} from '../utils/color';
 
-/**
- * 理论上的RYB色值
- * 基于https://blog.csdn.net/weixin_44938037/article/details/90599711 建立
- * 使用目前的自定义的mix算法，不能准确满足要求，故直接给颜色值。
- */
 const colorList = [
   '#FFFF00', '#FFAA00', '#FF8000', '#FF5500', 
   '#FF0000', '#FF0080', '#FF00FF', '#8080FF', 
   '#0080FF', '#00FFFF', '#00FF00', '#80FF00'
 ];
 
-// const colors = colorList.map(color => new Color(color));
-
-const baseList = ['#66B032', '#8601AF', '#ff8000'];
-const baseColor = baseList.map(color => new Color(color));
+const baseColor = colorList.map(color => new Color(color));
 const colors = make(baseColor, 1, mix);
-
-const lineWidth = 40;
-const r = 200;
-const around = 10;
-
-const x = r + lineWidth + around;
-const y = x;
-const radius = 2 * Math.PI / colors.length;
 
 /**
  * 顺时针绘制色相环
@@ -32,23 +16,68 @@ const radius = 2 * Math.PI / colors.length;
  * @param ctx CanvasRenderingContext2D
  */
 const ring = (colors: Color[], ctx: CanvasRenderingContext2D): void => {
-  const offset = (Math.PI + radius) / 2;
-  ctx.lineWidth = lineWidth;
+  
+};
 
-  colors.forEach((color, index) => {
-    const startAngle = index * radius - offset;
-    const endAngle = startAngle + radius;
+const edge = 20;
+const deep = 3;
+const x = (1 + deep * 3 / 2) * edge;
+const y = x;
 
-    ctx.beginPath();
-    ctx.strokeStyle = color.toRGBA();
-    ctx.arc(x, y, r, startAngle, endAngle);
-    ctx.stroke();
-    ctx.closePath();
+/**
+ * 根据正六边形中心点坐标，获取六个顶点坐标
+ * @param x 横坐标
+ * @param y 纵坐标
+ * @param e 边长
+ * @returns 六个顶点坐标
+ */
+const makePoints = (x: number, y: number, e: number) => {
+  return [
+    {
+      x: x + e / 2,
+      y: y - e * Math.sqrt(3) / 2,
+    },
+    {
+      x: x + e,
+      y,
+    },
+    {
+      x: x + e / 2,
+      y: y + e * Math.sqrt(3) / 2,
+    },
+    {
+      x: x - e / 2,
+      y: y + e * Math.sqrt(3) / 2,
+    },
+    {
+      x: x - e,
+      y,
+    },
+    {
+      x: x - e / 2,
+      y: y - e * Math.sqrt(3) / 2,
+    }
+  ];
+};
+
+const hexagon = (x: number, y: number, edge: number, color: Color, ctx: CanvasRenderingContext2D) => {
+  const points = makePoints(x, y, edge);
+  ctx.beginPath();
+  points.forEach(({x, y}, index) => {
+    if (index === 0) {
+      ctx.moveTo(x, y);
+    } else {
+      ctx.lineTo(x, y);
+    }
   });
+  ctx.fillStyle = color.toRGBA();
+  ctx.fill();
+  ctx.closePath();
 };
 
 const convert = (ctx: CanvasRenderingContext2D): void => {
-  ring(colors, ctx);
+  // ring(colors, ctx);
+  hexagon(x, y, edge, new Color('#FF0000'), ctx);
 };
 
 export {
