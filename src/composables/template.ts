d8ed10480c4f5e65ee2d60b76d6baff09a838c20
fuchunhui <fuchuhu7@gmail.com>
2021-12-28 16:54:51 +0800

    feat: remove template

--- src/composables/template.ts
+++
@@ -1,8 +0,0 @@
-const template = (ctx: CanvasRenderingContext2D): void => {
-  ctx.beginPath();
-  ctx.arc(200, 200, 100, 0, 2 * Math.PI);
-  ctx.fillStyle = '#FF0000';
-  ctx.fill();
-};
-
-export default template;
